88cc5cfc0093387482f76088586c4d4875189f7f
overkill489 <overkill.alt.489@gmail.com>
2026-07-08 17:57:32 -0500

    07/08/2026 @ 5:27pm

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 // http://www.omdbapi.com/?apikey=[7597afd]&
-//http://img.omdbapi.com/?apikey=[7597afd]&
+// http://img.omdbapi.com/?apikey=[7597afd]&
+// https://www.omdbapi.com/?apikey=7597afd&s=fast
+
+async function movies() {
+  const data = await fetch(`https://www.omdbapi.com/?apikey=7597afd&s=fast`);
+  const moviesData = await data.json();
+  moviesData
+    .map(
+      (movie) => ` <div class="movie">
+                <figure class="movie__img--wrapper">
+                  <img
+                    class="movie__img"
+                    src="${movie.Poster}"
+                    alt=""
+                  />
+                </figure>
+                <div class="movie__title">${movie.title}</div>
+                <div class="movie__year">
+                  ${movie.year}
+                </div>
+              </div>`,
+    )
+    .join("");
+}
+
+movies();
