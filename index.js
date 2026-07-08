2713e31e4d6e30a1a82ac5c9391590ef0b63b635
overkill489 <overkill.alt.489@gmail.com>
2026-07-08 18:54:02 -0500

    07/08/2026 @ 6:53pm

--- index.js
+++ index.js
@@ -7,25 +7,25 @@ const movieListEl = document.querySelector(".movie-list");
 async function movies() {
   const data = await fetch(`https://www.omdbapi.com/?apikey=7597afd&s=fast`);
   const moviesData = await data.json();
- movieListEl.innerHTML = moviesData
-    .map(
-      (movie) => movieHTML(movie)).join("");
-}
 
+  movieListEl.innerHTML = moviesData.Search.map((movie) =>
+    movieHTML(movie),
+  ).join("");
+}
 movies();
 
 function movieHTML(movie) {
-    return  ` <div class="movie">
-                <figure class="movie__img--wrapper">
-                  <img
-                    class="movie__img"
-                    src="${movie.Poster}"
-                    alt=""
-                  />
-                </figure>
-                <div class="movie__title">${movie.title}</div>
-                <div class="movie__year">
-                  ${movie.year}
-                </div>
-              </div>`
+  return `<div class="movie">
+            <figure class="movie__img--wrapper">
+              <img
+                class="movie__img"
+                src="${movie.Poster}"
+                alt=""
+              />
+            </figure>
+            <div class="movie__title">${movie.Title}</div>
+            <div class="movie__year">
+              ${movie.Year}
+            </div>
+          </div>`;
 }
