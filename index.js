8952325186552f0738dfb6ce447b33b2a623171b
overkill489 <overkill.alt.489@gmail.com>
2026-07-08 21:54:08 -0500

    07/08/2026 @ 9:54pm

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 // http://www.omdbapi.com/?apikey=[7597afd]&
 // http://img.omdbapi.com/?apikey=[7597afd]&
 // https://www.omdbapi.com/?apikey=7597afd&s=fast
-
 const movieListEl = document.querySelector(".movies");
+let moviesData = []; 
 
 async function movies() {
   const data = await fetch(`https://www.omdbapi.com/?apikey=7597afd&s=fast`);
-  const moviesData = await data.json();
-
-  movieListEl.innerHTML = moviesData.Search.map((movie) =>
-    movieHTML(movie),
-  ).join("");
+  const result = await data.json();
+  moviesData = result.Search;
+  renderMovies(moviesData);
 }
 movies();
+
+function renderMovies(list) {
+  movieListEl.innerHTML = list.map((movie) => movieHTML(movie)).join("");
+}
+
+function filterMovies(event) {
+  const sortType = event.target.value;
+  let sorted = [...moviesData]; 
+
+  if (sortType === "oldest-newest") {
+    sorted.sort((a, b) => a.Year - b.Year);
+  } else if (sortType === "newest-oldest") {
+    sorted.sort((a, b) => b.Year - a.Year);
+  }
+
+  renderMovies(sorted);
+}
 
 function movieHTML(movie) {
   return `<div class="movie">
